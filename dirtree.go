2e960e97c0b320c1bda6f76200c26e94688c541a
mwoss <wos.mateusz16@gmail.com>
2019-07-11 00:23:11 +0200

    Look better, a least a bit

--- dirtree.go
+++ dirtree.go
@@ -5,19 +5,35 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
-	err := filepath.Walk("D:\\Books",
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	startDepth := strings.Count(dir, "\\") + 2
+
+	err = filepath.Walk(dir,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			fmt.Println(path, info.Size())
+			last := strings.Split(path, "\\")
+
+
+			if len(last) == startDepth - 1{
+				fmt.Println(last[len(last)-1])
+			} else {
+				spaces := strings.Repeat(" ", (len(last) - startDepth) * 6)
+				fmt.Println(spaces, "├────", last[len(last)-1])
+			}
 			return nil
 		})
 
-	if err != nil{
+	if err != nil {
 		log.Println(err)
 	}
 }
